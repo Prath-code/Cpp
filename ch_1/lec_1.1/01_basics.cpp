282d548a7857e8abd7db9ea60de6e1c43ec70aaa
Prath Udhnawala <askforvarn@gmail.com>
2025-06-14 16:20:21 +0530

    Add basic C++ program and differences between C and C++

---
+++ ch_1/lec_1.1/01_basics.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+    int a, b, c;
+
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "Value of a: " << a << " + " << "Value of b: " << b << " = " << "Value of c: " << c << endl;
+
+    return 0;
+}
